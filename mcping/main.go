4310578e66b82675a75ee042cc13f7e7beefca41
Tnze <cjd001113@outlook.com>
2021-03-03 15:33:08 +0800

    Now we ping all addresses in SRV records.

--- mcping/main.go
+++ mcping/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net"
-	"os"
 	"strconv"
 
 	"github.com/go-mc/mcping"
@@ -18,63 +17,63 @@ var output = colorable.NewColorableStdout()
 
 func main() {
 	flag.Parse()
-	addr := flag.Arg(0)
+	addrs := lookupMC(flag.Arg(0))
 
-	if _, _, err := net.SplitHostPort(addr); err != nil {
-		if addrErr, ok := err.(*net.AddrError); ok && addrErr.Err == "missing port in address" {
-			addr = net.JoinHostPort(lookupMC(addr))
-		} else {
-			exit(fmt.Errorf("split address error: %v", err), 1)
+	for _, addr := range addrs {
+		fmt.Fprintf(output, "MCPING (%s):\n", addr)
+		conn, err := net.Dial("tcp", addr)
+		if err != nil {
+			fmt.Fprintf(output, "dial error: %v\n", err)
+			continue
 		}
-	}
+		status, delay, err := mcping.PingAndListConn(conn, *protocol)
+		if err != nil {
+			fmt.Fprintf(output, "error: %v\n", err)
+			continue
+		}
 
-	fmt.Fprintf(output, "MCPING (%s):\n", addr)
-	status, delay, err := mcping.PingAndList(addr, *protocol)
-	if err != nil {
-		exit(err, 1)
-	}
-
-	fmt.Fprintf(output,
-		`server: %s
-protocol: %d
-description: 
-%s
-delay: %v
-list: %d/%d
+		fmt.Fprintf(output,
+			`	server: %s
+	protocol: %d
+	description: 
+	%s
+	delay: %v
+	list: %d/%d
 `,
-		status.Version.Name,
-		status.Version.Protocol,
-		status.Description, delay,
-		status.Players.Online,
-		status.Players.Max)
-	for _, v := range status.Players.Sample {
-		fmt.Fprintf(output, "- [%s] %v\n", v.Name, v.ID)
-	}
-
-	if *favicon != "" {
-		fmt.Fprintf(output, "Save server's icon into %s\n", *favicon)
-		icon, err := status.Favicon.ToPNG()
-		if err != nil {
-			exit(err, 2)
+			status.Version.Name,
+			status.Version.Protocol,
+			status.Description, delay,
+			status.Players.Online,
+			status.Players.Max)
+		for _, v := range status.Players.Sample {
+			fmt.Fprintf(output, "- [%s] %v\n", v.Name, v.ID)
 		}
-		err = ioutil.WriteFile(*favicon, icon, 0666)
-		if err != nil {
-			exit(err, 1)
+
+		if *favicon != "" {
+			fmt.Fprintf(output, "Save server's icon into %s\n", *favicon)
+			icon, err := status.Favicon.ToPNG()
+			if err != nil {
+				fmt.Fprintf(output, "error: %v\n", err)
+				continue
+			}
+			err = ioutil.WriteFile(*favicon, icon, 0666)
+			if err != nil {
+				fmt.Fprintf(output, "error: %v\n", err)
+				continue
+			}
 		}
 	}
 }
 
 // written after read mojang's code
-func lookupMC(addr string) (host, port string) {
-	_, addrs, err := net.LookupSRV("minecraft", "tcp", addr)
-	if err == nil && len(addrs) > 0 && addrs[0] != nil {
-		return addrs[0].Target, strconv.Itoa(int(addrs[0].Port))
+func lookupMC(addr string) (addrs []string) {
+	_, addrsSRV, err := net.LookupSRV("minecraft", "tcp", addr)
+	if err == nil && len(addrsSRV) > 0 {
+		for _, addrSRV := range addrsSRV {
+			addrs = append(addrs, net.JoinHostPort(addrSRV.Target, strconv.Itoa(int(addrSRV.Port))))
+		}
+	} else {
+		addrs = []string{net.JoinHostPort(addr, "25565")}
 	}
-	// lookup SRV error, return with default port
-	return addr, "25565"
-}
-
-func exit(err error, code int) {
-	fmt.Fprintf(output, "error: %v\n", err)
-	os.Exit(code)
+	return
 }
